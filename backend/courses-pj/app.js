8a56991a1c2712bc4503bfa5c2f9665938f6b736
bassant <bassantelboray@gmail.com>
2025-09-27 18:21:23 +0300

    Fix backend ES module error (require to import, add __dirname)

--- backend/courses-pj/app.js
+++ backend/courses-pj/app.js
@@ -1,16 +1,21 @@
 import app from './src/app.js';
 import { environment } from './src/config/server.config.js';
 
-// Start local server only when running outside Vercel (e.g., npm run start)
-const path = require('path');
+import path from 'path';
+
+import express from 'express';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, '..', 'frontend', 'build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'frontend', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, '..', 'frontend', 'build', 'index.html'));
 });
 
 const port = environment.SERVER_PORT;
 app.listen(port, () => {
-	console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
